abdb22af1bde7e93dccfbaaad6f525d17a06e93b
Evandro Ribeiro <ribeiroevandro@live.com>
2018-10-16 03:03:50 -0300

    Add ReactToTron for debug

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import './src/config/ReacttotronConfig';
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
